585e5cd8d2f52b028dd94965f41ee65e46ecf306
wasif789 <wsffarid1@gmail.com>
2021-10-03 21:58:52 +0530

    appending

--- Operations.cs
+++ Operations.cs
@@ -41,6 +41,31 @@ namespace LinkedListOperation
             }
             return temp;
         }
+        //Appending a new node
+        public void Append(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            new_node.data = new_data;//assign data element
+            new_node.next = null;//assign null to next of new node
+            //check linkedlist empty 
+            if (head == null)
+            {
+                head = new_node;
+
+            }
+            else
+            {
+                //traverse to last node
+                Node temp = new Node(new_data);
+                temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = new_node;
+
+            }
+        }
         //Display the nodes
         public void Display()
         {
--- Program.cs
+++ Program.cs
@@ -9,11 +9,11 @@ namespace LinkedListOperation
             //UseCase 1
             Operations obj = new Operations();
             //Insertion Operation
-            obj.InsertFront(56);
-            obj.InsertFront(30);
-            obj.InsertFront(70);
+            obj.Append(56);
+            obj.Append(30);
+            obj.Append(70);
             //Display Operation
-            Console.WriteLine("*****Insertion at Last*****");
+            Console.WriteLine("*****Appending*****");
             obj.Display();
         }
     }
